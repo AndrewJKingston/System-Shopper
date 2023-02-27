79e99d7f2eaec65cd3336d78f82d65d1f94c8c6e
TromboneSamurai <andrewslugger@gmail.com>
2023-02-27 15:44:03 -0600

    Created database

---
+++ System Shopper DB/dbo/Tables/Manufacturer.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Manufacturer] (
+    [ManufacturerID]   INT           IDENTITY (1, 1) NOT NULL,
+    [ManufacturerName] VARCHAR (100) NOT NULL,
+    CONSTRAINT [PK_Manufacturer] PRIMARY KEY CLUSTERED ([ManufacturerID] ASC)
+);
+
