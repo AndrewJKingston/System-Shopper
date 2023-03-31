e66e9a22b6897e8404174b4ef0d0d2a1b81242d6
TromboneSamurai <andrewslugger@gmail.com>
2023-03-31 09:50:38 -0500

    Working on adding multiple models to product page, EditManufacturer page in progress

---
+++ System Shopper/Pages/Manufacturers/EditManufacturer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace System_Shopper.Pages.Manufacturers
+{
+    public class EditManufacturerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
