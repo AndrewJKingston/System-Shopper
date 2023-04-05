c3db349c069fa7d767947a11b0357d79fd7ddece
TromboneSamurai <andrewslugger@gmail.com>
2023-04-05 15:34:43 -0500

    Updated Manufacturer dropdown in AddProduct

--- System Shopper/Pages/AddProduct.cshtml.cs
+++ System Shopper/Pages/AddProduct.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Data.SqlClient;
 using System_Shopper.Models;
 
@@ -15,41 +16,31 @@ namespace System_Shopper.Pages
         [BindProperty]
         public List<Manufacturer> GetManufacturerList { get; set; } = new List<Manufacturer>();
 
+        [BindProperty]
+        public List<SelectListItem> Manufacturers { get; set; } = new List<SelectListItem>();
+
         public void OnGet()
         {
-            /*
-             * 1. Create a SQL connection object
-             * 2. Construct a SQL statement
-             * 3. Create a SQL command object
-             * 4. Open the SQL connection
-             * 5. Execute the SQL command
-             * 6. Close the SQL connection
-             */
+            PopulateManufacturers();
+        }
 
-            // Step 1
+        private void PopulateManufacturers()
+        {
             using (SqlConnection conn = new SqlConnection(DBHelper.GetConnectionString()))
             {
-                // Step 2
                 string sql = "SELECT * FROM Manufacturer ORDER BY ManufacturerName";
-
-                // Step 3
                 SqlCommand cmd = new SqlCommand(sql, conn);
 
-                // Step 4
                 conn.Open();
-
-                // Step 5
                 SqlDataReader reader = cmd.ExecuteReader();
                 if (reader.HasRows)
                 {
                     while (reader.Read())
                     {
-                        Manufacturer manufacturer = new Manufacturer();
-                        manufacturer.ManufacturerName = reader["ManufacturerName"].ToString();
-                        manufacturer.ManufacturerBio = reader["ManufacturerBio"].ToString();
-                        manufacturer.ManufacturerId = int.Parse(reader["ManufacturerId"].ToString());
-                        manufacturer.ManufacturerLogo = reader["ManufacturerLogo"].ToString();
-                        GetManufacturerList.Add(manufacturer);
+                        SelectListItem manufacturer = new SelectListItem();
+                        manufacturer.Value = reader["ManufacturerId"].ToString();
+                        manufacturer.Text = reader["ManufacturerName"].ToString();
+                        Manufacturers.Add(manufacturer);
                     }
                 }
             }
