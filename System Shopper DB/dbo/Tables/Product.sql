5a8b669b4c49e4fccfcad18c4a9c879d2f6efa9f
TromboneSamurai <andrewslugger@gmail.com>
2023-04-10 01:06:45 -0500

    Fixed price not displaying as decimal, still working on System List

--- System Shopper DB/dbo/Tables/Product.sql
+++ System Shopper DB/dbo/Tables/Product.sql
@@ -1,15 +1,17 @@
 ﻿CREATE TABLE [dbo].[Product] (
-    [ProductID]          INT            IDENTITY (1, 1) NOT NULL,
-    [ProductName]        VARCHAR (300)  NOT NULL,
-    [ProductDescription] VARCHAR (1000) NOT NULL,
-    [ManufacturerID]     INT            NOT NULL,
-    [Price]              DECIMAL (18)   NOT NULL,
-    [DiscountID]         INT            NULL,
-    [ProductImage]       VARCHAR (300)  NULL,
+    [ProductID]          INT             IDENTITY (1, 1) NOT NULL,
+    [ProductName]        VARCHAR (300)   NOT NULL,
+    [ProductDescription] VARCHAR (1000)  NOT NULL,
+    [ManufacturerID]     INT             NOT NULL,
+    [Price]              DECIMAL (18, 2) NOT NULL,
+    [DiscountID]         INT             NULL,
+    [ProductImage]       VARCHAR (300)   NULL,
     CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED ([ProductID] ASC),
     CONSTRAINT [FK_Product_Discount] FOREIGN KEY ([DiscountID]) REFERENCES [dbo].[Discount] ([DiscountID]) ON DELETE SET NULL,
     CONSTRAINT [FK_Product_Manufacturer] FOREIGN KEY ([ManufacturerID]) REFERENCES [dbo].[Manufacturer] ([ManufacturerID]) ON DELETE CASCADE
 );
+
+
 
 
 
