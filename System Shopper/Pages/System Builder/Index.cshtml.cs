e73a029e6709086932ed8e3b4ef5901cfa6bc7e5
TromboneSamurai <andrewslugger@gmail.com>
2023-04-09 20:27:32 -0500

    Created System Builder page, working on grid layout

---
+++ System Shopper/Pages/System Builder/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace System_Shopper.Pages.System_Builder
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
