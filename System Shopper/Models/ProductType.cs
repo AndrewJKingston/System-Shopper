2fd959fcf92c1324d172f687c558716b3f044961
smcraven <smcraven2@gmail.com>
2023-04-24 16:38:11 -0500

    added Product Type tab to main page

---
+++ System Shopper/Models/ProductType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace System_Shopper.Models
+{
+    public class ProductType
+    {
+        [Key]
+        public int ProductTypeId { get; set; }
+
+        [StringLength(100)]
+        public string ProductTypeName { get; set; } = string.Empty;
+    }
+}
---
+++ System Shopper/Pages/ProductTypes/ProductTypeIndex.cshtml.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using System_Shopper.Models;
+using System.Collections.Generic;
+
+namespace System_Shopper.Pages.ProductTypes
+{
+    public class ProductTypeIndex : PageModel
+    {
+        [BindProperty]
+        public ProductType NewProductType { get; set; } = new ProductType();
+
+        [BindProperty]
+        public List<ProductType> GetProductTypeList { get; set; } = new List<ProductType>();
+
+        public void OnGet()
+        {
+            // Step 1
+            using (SqlConnection conn = new SqlConnection(DBHelper.GetConnectionString()))
+            {
+                // Step 2
+                string sql = "SELECT ProductType FROM ProductType";
+
+                // Step 3
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                // Step 4
+                conn.Open();
+
+                // Step 5
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        ProductType productType = new ProductType();
+                        productType.ProductTypeName = reader["ProductType"].ToString();
+
+                        GetProductTypeList.Add(productType);
+                    }
+                }
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                // Step 1
+                using (SqlConnection conn = new SqlConnection(DBHelper.GetConnectionString()))
+                {
+                    // Step 2
+                    string sql = "INSERT INTO ProductType(ProductType)" + "VALUES(@ProductType)";
+
+                    // Step 3
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@ProductType", NewProductType.ProductTypeName);
+
+                    // Step 4
+                    conn.Open();
+
+                    // Step 5
+                    cmd.ExecuteNonQuery();
+                }
+                return RedirectToPage("/Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
