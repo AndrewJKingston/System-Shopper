353a49470d57b7e990de98feb565ca19d6d3b35f
MaxDunham8 <dunhammax@gmail.com>
2023-04-30 21:28:07 -0500

    added cart page

---
+++ System Shopper/Pages/Cart/Cart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace System_Shopper.Pages.Cart
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
