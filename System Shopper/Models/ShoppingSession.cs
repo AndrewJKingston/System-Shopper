0861ab80519216bb76df6a51f1bf8f14dcd8d4ee
TromboneSamurai <andrewslugger@gmail.com>
2023-05-02 18:46:05 -0500

    Working on System Builder product list connection

---
+++ System Shopper/Models/ShoppingSession.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace System_Shopper.Models
+{
+    public class ShoppingSession
+    {
+        [Key]
+        public int ShoppingSessionId { get; set; }
+
+        public int UserId { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public int SessionId { get; set; }
+    }
+}
