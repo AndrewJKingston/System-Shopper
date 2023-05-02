318046832ec031dceda24487910ee4e2e712bad2
TromboneSamurai <andrewslugger@gmail.com>
2023-05-02 18:46:56 -0500

    Update Cart.sql

--- System Shopper DB/dbo/Tables/Cart.sql
+++ System Shopper DB/dbo/Tables/Cart.sql
@@ -3,9 +3,11 @@
     [ShoppingSessionID] INT      NOT NULL,
     [ProductID]         INT      NOT NULL,
     [Quantity]          INT      NOT NULL,
-    [CreatedAt]         DATETIME NOT NULL,
+    [CreatedAt]         DATETIME NULL,
     CONSTRAINT [PK_Cart] PRIMARY KEY CLUSTERED ([CartID] ASC),
     CONSTRAINT [FK_Cart_Cart] FOREIGN KEY ([ProductID]) REFERENCES [dbo].[Product] ([ProductID]),
     CONSTRAINT [FK_Cart_ShoppingSession] FOREIGN KEY ([ShoppingSessionID]) REFERENCES [dbo].[ShoppingSession] ([ShoppingSessionID])
 );
+
+
 
